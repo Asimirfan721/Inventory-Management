ef9186c9bafebca1b21c5946df1297c888aeea73
Asim Irfan <rajputasim721@gmail.com>
2025-04-09 12:06:26 +0500

    Currency Logic updated

--- app/Http/Controllers/CurrencyController.php
+++ app/Http/Controllers/CurrencyController.php
@@ -1,10 +1,29 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Currency;
 
-class CurrencyController
+class CurrencyController extends Controller
 {
-    //
+    public function index()
+    {
+        $currencies = Currency::all();
+        return view('currency.index', compact('currencies'));
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'code' => 'required|string|max:10',
+            'currency' => 'required|string|max:255',
+        ]);
+
+        Currency::create([
+            'code' => $request->code,
+            'currency' => $request->currency,
+        ]);
+
+        return redirect()->route('currency.index')->with('success', 'Currency added successfully!');
+    }
 }
