9fbf95eabec1905e601fd69dda963edfbd330d3a
Asim Irfan <rajputasim721@gmail.com>
2025-04-09 12:13:22 +0500

    Blade and routes are set for currency

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\CurrencyController; 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/currencies', [CurrencyController::class, 'index'])->name('currency.index');
+Route::post('/currencies', [CurrencyController::class, 'store'])->name('currency.store');
