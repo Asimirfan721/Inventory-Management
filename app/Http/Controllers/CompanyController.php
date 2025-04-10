9b9aff76676234cd774037f25c9ad1d3bccde753
Asim Irfan <rajputasim721@gmail.com>
2025-04-10 11:10:00 +0500

    controller company updated

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -1,10 +1,47 @@
 <?php
 
+<?php
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Company;
 
 class CompanyController extends Controller
 {
-    //
+   public function index()
+{
+    $companies = Company::all();
+    return view('company.index', compact('companies'));
+}
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'code' => 'required|string|max:10',
+            'companies' => 'required|string|max:255',
+        ]);
+
+        Companies::create([
+            'code' => $request->code,
+            'company' => $request->company,
+        ]);
+
+        return redirect()->route('company.index')->with('success', 'Company added successfully!');
+    }
+    public function update(Request $request, $id)
+{
+    $request->validate([
+        'code' => 'required|string|max:10',
+        'company' => 'required|string|max:255',
+    ]);
+
+    $company = Company::findOrFail($id);
+    $company->update([
+        'code' => $request->code,
+        'company' => $request->company,
+    ]);
+
+    return redirect()->route('company.index')->with('success', 'Company updated successfully!');
+}
+
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CurrencyController; 
+use App\Http\Controllers\CompanyController;
+
 Route::get('/', function () {
     return view('home');
 });
