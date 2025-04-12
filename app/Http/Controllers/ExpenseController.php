dbe60886252ed50352d89d3f293b074568092424
Asim Irfan <rajputasim721@gmail.com>
2025-04-12 12:46:48 +0500

    expense controller udpated

---
+++ app/Http/Controllers/ExpenseController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Expense;
+
+class ExpenseController extends Controller
+{
+    public function index(){
+        $expenses = Expense()::all();
+        return view('expense.index', compact('expenses'));
+    }
+}
