63809a967c6d614dbb44fff474597002a289d894
Asim Irfan <rajputasim721@gmail.com>
2025-04-13 18:10:13 +0500

    view completed

--- resources/views/account/index.blade.php
+++ resources/views/account/index.blade.php
@@ -25,24 +25,25 @@
       <tr>
         <td>{{ $index + 1 }}</td>
         <td>{{ $account->name }}</td>
-        <td>{{ $account->email }}</td>
         <td>{{ $account->phone }}</td>
-        <td>{{ $account->address }}</td>
+        <td>{{ $account->description }}</td>
+        <td>{{ $account->balance }}</td>
+        <td>{{ $account->status }}</td>
         <td>
-          <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editModal{{ $customer->id }}">Edit</button>
-          <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteModal{{ $customer->id }}">Delete</button>
+          <button class="btn btn-sm btn-primary" data-toggle="modal" data-target="#editModal{{ $account->id }}">Edit</button>
+          <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#deleteModal{{ $account->id }}">Delete</button>
         </td>
       </tr>
 
       <!-- Edit Modal -->
-      <div class="modal fade" id="editModal{{ $customer->id }}" tabindex="-1" role="dialog" aria-labelledby="editModalLabel{{ $customer->id }}" aria-hidden="true">
+      <div class="modal fade" id="editModal{{ $account->id }}" tabindex="-1" role="dialog" aria-labelledby="editModalLabel{{ $account->id }}" aria-hidden="true">
         <div class="modal-dialog" role="document">
-          <form action="{{ route('customer.update', $customer->id) }}" method="POST">
+          <form action="{{ route('account.update', $account->id) }}" method="POST">
             @csrf
             @method('PUT')
             <div class="modal-content">
               <div class="modal-header">
-                <h5 class="modal-title">Edit Customer</h5>
+                <h5 class="modal-title">Edit account</h5>
                 <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                   <span>&times;</span>
                 </button>
@@ -50,20 +51,25 @@
               <div class="modal-body">
                 <div class="form-group">
                   <label>Name</label>
-                  <input type="text" name="name" class="form-control" value="{{ $customer->name }}" required>
-                </div>
-                <div class="form-group">
-                  <label>Email</label>
-                  <input type="email" name="email" class="form-control" value="{{ $customer->email }}" required>
+                  <input type="text" name="name" class="form-control" value="{{ $account->name }}" required>
                 </div>
                 <div class="form-group">
                   <label>Phone</label>
-                  <input type="text" name="phone" class="form-control" value="{{ $customer->phone }}" required>
+                  <input type="text" name="phone" class="form-control" value="{{ $account->email }}" required>
+                </div>
+                <div class="form-group">
+                  <label>Description</label>
+                  <input type="description" name="description" class="form-control" value="{{ $account->phone }}" required>
                 </div>
                 <div class="form-group">
                   <label>Address</label>
-                  <input type="text" name="address" class="form-control" value="{{ $customer->address }}" required>
+                  <input type="text" name="address" class="form-control" value="{{ $account->address }}" required>
                 </div>
+                <div class="form-group">
+                  <label>Balance</label>
+                  <input type="text" name="balance" class="form-control" value="{{ $account->address }}" required>
+                </div>
+                
               </div>
               <div class="modal-footer">
                 <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
@@ -78,14 +84,14 @@
   </table>
 </div>
 
-<!-- Add Customer Modal -->
-<div class="modal fade" id="addCustomerModal" tabindex="-1" role="dialog" aria-labelledby="addCustomerModalLabel" aria-hidden="true">
+<!-- Add account Modal -->
+<div class="modal fade" id="addaccountModal" tabindex="-1" role="dialog" aria-labelledby="addaccountModalLabel" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <form action="{{ route('customer.store') }}" method="POST">
+    <form action="{{ route('account.store') }}" method="POST">
       @csrf
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="addSCustomerModalLabel">Add Customer</h5>
+          <h5 class="modal-title" id="addSaccountModalLabel">Add account</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span>&times;</span>
           </button>
@@ -118,21 +124,21 @@
 </div>
  
 <!-- Delete Modals -->
-@foreach($customers as $customer)
-<div class="modal fade" id="deleteModal{{ $customer->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel{{ $customer->id }}" aria-hidden="true">
+@foreach($accounts as $account)
+<div class="modal fade" id="deleteModal{{ $account->id }}" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel{{ $account->id }}" aria-hidden="true">
   <div class="modal-dialog" role="document">
-    <form action="{{ route('customer.destroy', $customer->id) }}" method="POST">
+    <form action="{{ route('account.destroy', $account->id) }}" method="POST">
       @csrf
       @method('DELETE')
       <div class="modal-content">
         <div class="modal-header">
-          <h5 class="modal-title" id="deleteModalLabel{{ $customer->id }}">Delete Customer</h5>
+          <h5 class="modal-title" id="deleteModalLabel{{ $account->id }}">Delete account</h5>
           <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span>&times;</span>
           </button>
         </div>
         <div class="modal-body">
-          Are you sure you want to delete this Customer?
+          Are you sure you want to delete this account?
         </div>
         <div class="modal-footer">
           <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
