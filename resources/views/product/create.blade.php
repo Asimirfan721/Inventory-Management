d22468505e3604ab102b0b13ab331ebb2d4d698a
Asim Irfan <rajputasim721@gmail.com>
2025-04-17 09:45:59 +0500

    migrations updated, company_id added in both suppliers and customers

--- resources/views/product/create.blade.php
+++ resources/views/product/create.blade.php
@@ -2,7 +2,7 @@
 
 @section('content') 
 <div class="container">
-    <h4 class="mb-4">Add New Currency</h4>
+    <h4 class="mb-4">Add New Product</h4>
 
     <a href="{{ url('/') }}" class="btn btn-secondary mb-3">Home</a>
     <a href="{{ route('product.index') }}" class="btn btn-primary mb-3">Back to Product Page</a>
