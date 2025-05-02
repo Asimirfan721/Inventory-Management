9cbf3d7867604bc0d193a7116c3a7c36e3b87e97
Asimirfan721 <rajputasim721@gmail.com>
2025-05-02 23:56:33 +0500

    company added in inventory

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 use App\Models\Brand;
 use Illuminate\Http\Request;
 use App\Models\Product;
-use App\Models\Company;
+use App\Models\Company; //company model
 
 class BrandController extends Controller
 {
