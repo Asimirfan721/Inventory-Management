3d382b5dc5f3e171335cf43919807e3ec1433ca3
Asimirfan721 <rajputasim721@gmail.com>
2025-05-07 23:58:29 +0500

    Model

--- app/Http/Controllers/StockController.php
+++ app/Http/Controllers/StockController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Stock;
-use App\Models\Supplier;
+use App\Models\Supplier; // model
 
 class StockController extends Controller
 {
