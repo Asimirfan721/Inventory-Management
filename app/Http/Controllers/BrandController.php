dfc32b8c4f0aab97df02fde9919a88c82d32f298
Asim Irfan <rajputasim721@example.com>
2025-05-12 20:53:17 +0500

    validation added

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -16,7 +16,7 @@ class BrandController extends Controller
     }
     public function store(Request $request)
 {
-    $request->validate([ 
+    $request->validate([   //validation
         'title' => 'required|string|max:255',
         'summary' => 'required|string|max:255',
     ]);
