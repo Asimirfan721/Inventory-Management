f32893aa28684e44b5a3173e247cf4293557cec0
Asim Irfan <asim@example.com>
2025-05-12 19:13:04 +0500

    first commit from inventory

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 use App\Models\Brand;
 use Illuminate\Http\Request;
-use App\Models\Product;
+use App\Models\Product; // model
 use App\Models\Company; //company model
 use App\Models\Currency; //currency model
 
