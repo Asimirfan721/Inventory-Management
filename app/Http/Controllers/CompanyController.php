af7235a02f8e9599bb8b7101b5ca2437d977ec9e
Asimirfan721 <rajputasim721@gmail.com>
2025-06-17 23:43:53 +0500

    logo and names were not visible, made changs in databse as well

--- app/Http/Controllers/CompanyController.php
+++ app/Http/Controllers/CompanyController.php
@@ -11,7 +11,7 @@ class CompanyController extends Controller
     public function index()
     {
         $companies = Company::with('currency')->get(); // eager load currency
-        $currencies = Currency::all();
+        $currencies = Currency::all();   // Not loading currency, it's not being stored in database
         return view('company.index', compact('companies', 'currencies'));
     }
 
